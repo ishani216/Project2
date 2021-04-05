09ed4e080447b292371de1a668d3c47307675529
ishani216 <31428274+ishani216@users.noreply.github.com>
2021-04-04 23:00:13 -0400

    Add project files.

---
+++ Project2/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Project2.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project2.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            Price price = new Price();
+            //var session = new PriceSession(HttpContext.Session);
+            //session.SetPrice(price);
+            return View(price); ;
+        }
+
+        [HttpPost]
+        public IActionResult Index(Price price)
+        {
+            price.CalculateTotal();
+            //var session = new PriceSession(HttpContext.Session);
+            //session.SetPrice(price);
+            return View(price);
+        }
+    }
+}
